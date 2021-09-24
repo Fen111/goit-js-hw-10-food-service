d62e073b3cf5d4a72eafd6fc15c856f4167d670e
Viktor Sinelnykov <Fenya.ua@gmail.com>
2021-09-24 20:27:21 +0300

    исправление ошибок

--- src/js/switch-themes.js
+++ src/js/switch-themes.js
@@ -8,33 +8,17 @@ const body = document.querySelector('body');
 checkbox.addEventListener('change', changeTheme);
 
 function changeTheme() {
-  //   body.classList.toggle(DARK);
+  body.classList.toggle(DARK);
 
-  //   if (body.className === '') {
-  //     body.classList.add(LIGHT);
-  //   } else {
-  //     body.classList.remove(LIGHT);
-  //   }
-
-  if (checkbox.checked) {
-    body.classList.add(DARK);
-    body.classList.remove(LIGHT);
-  } else {
-    body.classList.add(LIGHT);
-    body.classList.remove(DARK);
-  }
+  !checkbox.checked ? body.classList.add(LIGHT) : body.classList.remove(LIGHT);
 
   localStorage.setItem('theme', body.className);
 }
 
 function currentTheme() {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme) {
-    body.className = savedTheme;
-  }
-  if (savedTheme === DARK) {
-    checkbox.checked = true;
-  }
+  const savedTheme = localStorage.getItem('theme') || LIGHT;
+  body.className = savedTheme;
+  checkbox.checked = savedTheme === DARK;
 }
 
 currentTheme();
